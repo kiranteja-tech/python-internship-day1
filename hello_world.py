7b9dca55b432a9625b5db6283cb1c909ecc130e7
kiranteja-tech <kiranteja5912@gmail.com>
2026-01-15 17:21:02 +0530

    Add files via upload

---
+++ hello_world.py
@@ -0,0 +1,17 @@
+# This program prints user details for internship task
+# It also uses variables, user input, and comments
+
+# Taking user input for name
+name = input("Enter your name: ")
+
+# Taking user input for internship role
+role = input("Enter your internship role: ")
+
+# Storing today's date in a variable
+date = "16 January 2026"
+
+# Printing the output using variables
+print("\n--- Internship Task Output ---")
+print("Name:", name)
+print("Role:", role)
+print("Date:", date)
